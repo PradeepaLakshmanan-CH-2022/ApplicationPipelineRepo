f5277925be724a9e8ba3b3d668e9b16c240e127a
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-12 18:32:28 +0530

    name

--- AWSCOnsole/Hello.cs
+++ AWSCOnsole/Hello.cs
@@ -10,7 +10,7 @@ namespace AWSCOnsole
     {
         public string GetName()
         {
-            string name = "Hello Kavi,Pipeline created Successful done";
+            string name = "Hello Kavi,Pipeline created Successful done Good";
             Console.WriteLine(name); 
             return name;    
         }
